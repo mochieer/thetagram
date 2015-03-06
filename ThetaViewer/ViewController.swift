a3d51ae12f25aae8dfa73db1a4ab3ebcce8c06d0
rynagash <nagashi.nishi@gmail.com>
2015-03-06 14:27:19 +0900

    Initial Commit

---
+++ ThetaViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThetaViewer
+//
+//  Created by rynagash on 2015/03/06.
+//  Copyright (c) 2015年 rynagash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
